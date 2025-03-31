2e9a197bb139e7bca5bfa903f0c239ccf01d3cfd
Kilaparthi Ravi Teja <142376333+KilaparthiRaviTeja@users.noreply.github.com>
2025-03-31 09:11:10 +0530

    Update database.py

--- ai-agent-backend/database.py
+++ ai-agent-backend/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = "mongodb+srv://ravi:ravi@cluster0.bdi0mhr.mongodb.net/app_db?retryWrites=true&w=majority"
+MONGO_URI="mongodb+srv://ravi:bunny@cluster0.m6iwsdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = AsyncIOMotorClient(MONGO_URI)
 database = client.app_db  # This ensures we are using the 'app_db' database
